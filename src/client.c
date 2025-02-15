58130e15bb949b2c1b1ee4a8b6ace8276d470eb9
Mischa Poplow <horrikohl.dx@gmail.com>
2025-02-15 21:56:56 +0100

    more stuff

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: mpoplow <mpoplow@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 13:42:06 by mpoplow           #+#    #+#             */
-/*   Updated: 2025/02/15 21:03:46 by mpoplow          ###   ########.fr       */
+/*   Updated: 2025/02/15 21:54:33 by mpoplow          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 int	main(int argc, char *argv[])
 {
-	char *text;
-	if (argc != 3)
-		return (write(1, "wrong input\n", 12), 1);
+	char				*text;
+	struct sigaction	act;
+
+	if (argc != 3 || ft_strlen(argv[2]) < 1 || ft_strlen(argv[2]) > 5)
+		return (write(1, "Error\n", 6), 1);
 	text = argv[2];
-	while()
+	while (*text)
 	{
 		kill((pid_t)atoi(argv[1]), SIGUSR1);
+		text++;
 	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: mpoplow <mpoplow@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:06:02 by mpoplow           #+#    #+#             */
-/*   Updated: 2025/02/15 21:02:31 by mpoplow          ###   ########.fr       */
+/*   Updated: 2025/02/15 21:56:10 by mpoplow          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,20 @@
 // 	write(1, "\n", 1);
 // }
 
-void abc(int sig)
+void	abc(int sig)
 {
 	ft_putnbr_fd(sig, 1);
 	write(1, "\nBonobo\n", sizeof("\nBonobo\n"));
 }
 
-int	main(int argc, char *argv[])
+int	main(void)
 {
 	int	i;
-	struct sigaction act;
-	
+
 	ft_putnbr_fd(getpid(), 1);
-	(void)argc;
-	(void)argv;
-	(void)act;
 	i = 0;
+	signal(SIGUSR1, abc);
 	while (1)
-	{
-		signal(SIGUSR1, abc);
-		i = sleep(6000000);
-		// if (i != 0)
-	}
+		i = sleep(1);
 	return (0);
 }
